58205f7a74594e986161f5bed4d5d722134f3877
AustinPotts <pottsevolvements@gmail.com>
2019-10-14 14:45:37 -0400

    Configured Storyboard Layout/ Outlets/ Subclasses

--- iOS10-JournalApp/ViewControllers/EntryDetailViewController.swift
+++ iOS10-JournalApp/ViewControllers/EntryDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EntryDetailViewController.swift
 //  iOS10-JournalApp
 //
 //  Created by Austin Potts on 10/14/19.
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EntryDetailViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var entryTextField: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
-
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
 }
-
